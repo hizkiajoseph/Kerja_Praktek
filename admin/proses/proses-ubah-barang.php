6bc9533ccaa96c659e79899ca035bca1f6c3cd5f
hizkiajoseph <73878002+hizkiajoseph@users.noreply.github.com>
2020-11-18 23:38:14 +0800

    Hari-2.2
    semua fungsi CRUD sudah jadi

---
+++ admin/proses/proses-ubah-barang.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once '../../config/db.php';
+
+if (!isset($_SESSION['id_user'])) {
+  header('Location: ../../index.php');
+}
+
+$id = $_POST['id'];
+$nama_barang = $_POST['nama_barang'];
+$jenis = $_POST['jenis'];
+$jumlah = $_POST['jumlah'];
+$kondisi = $_POST['kondisi'];
+$keterangan = $_POST['keterangan'];
+$id_lab = $_POST['id_lab'];
+$tgl_regis = date('Y-m-d');
+$id_user = $_SESSION['id_user'];
+
+$update = $conn->query("UPDATE barang SET 
+   nama_barang = '$nama_barang',
+   jenis = '$jenis',
+   jumlah = '$jumlah',
+   kondisi = '$kondisi',
+   keterangan = '$keterangan',
+   id_lab = '$id_lab'
+   WHERE id_barang = '$id'");
+
+if ($update) {
+  header('Location: ../data-barang.php');
+} else {
+  header('Location: ../data-barang.php?h=edit-barang');
+}
