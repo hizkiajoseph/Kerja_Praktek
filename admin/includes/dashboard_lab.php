5a9b90424155ec162756efa4eeb123236148f2f5
hizkiajoseph <73878002+hizkiajoseph@users.noreply.github.com>
2020-11-26 17:36:09 +0800

    Hari-11
    Membuat Dasboard Lab

--- admin/includes/dashboard_lab.php
+++ admin/includes/dashboard_lab.php
@@ -1,26 +1,28 @@
 <div class="container mt-5">
 
   <div class="row text-center">
-    <div class="col-md-4">
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
  
           <?php 
-          $sql = $conn->query("SELECT COUNT(*) AS jmlBarang FROM barang");
-          $barang = $sql->fetch_assoc();
-          ?>
-          <?php
-          $id_lab = 1;
-          ?>
-          
-          <h5 class="card-title">Laboratorium Central</h5>
-          <p class="card-text">Jumlah barang saat ini</p>
-          <h3><?= $barang['jmlBarang']; ?></h3>
-          <a href="data-barang.php?id=<?php echo $id_lab?>" class="card-link">Lihat Data Barang</a>
-          
-        </div>
-      </div>
-    </div>
+          $sql = $conn->query("SELECT * FROM laboratorium");
+
+          while ($lab = $sql->fetch_assoc()) { 
+            $sqlbarang = $conn->query("SELECT count(*) AS jumlah FROM barang WHERE id_lab = '$lab[id_lab]'");
+            $barang = $sqlbarang->fetch_assoc();
+            echo "
+            <div class='col-md-4'>
+            <div class='card' style='width: 18rem;''>
+            <div class='card-body'>
+            <h5 class='card-title'>$lab[nama_lab]</h5>
+            <p class='card-text'>Jumlah barang saat ini</p>
+            <h3>$barang[jumlah]</h3>
+            <a href='data-barang.php?id=$lab[id_lab]' class='card-link'>Lihat Data Barang</a>
+            </div>
+            </div>
+            </div>";
+          }
+          ?>          
+   
+      
   </div>
 
 </div>
